1830284e87c1b597eb7449c61ecba7273e8a8da2
Michael Jones <michael.jones.gsy@gmail.com>
2024-07-10 12:49:43 +0100

    implements Iterator for lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -15,7 +15,7 @@ impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
             input: input.chars().peekable(),
-            line: 0,
+            line: 1,
             col: 0,
         }
     }
@@ -28,11 +28,66 @@ impl<'a> Iterator for Lexer<'a> {
         self.col += 1;
 
         if let Some(output_char) = self.input.next() {
-            Some(Token::new(
-                TokenType::Identifier(output_char.to_string()),
-                self.line,
-                self.col,
-            ))
+            if output_char.is_alphabetic() {
+                let start_col = self.col;
+
+                let mut temp_string = String::new();
+                temp_string.push(output_char);
+
+                // TODO: there's got to be a nicer way of doing this, come back and fix it.
+                while let Some(peeked) = self.input.peek() {
+                    if peeked.is_alphabetic() {
+                        temp_string.push(self.input.next().unwrap());
+                        self.col += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                let token_type = if temp_string == "fn" {
+                    TokenType::Function
+                } else if temp_string == "let" {
+                    TokenType::Let
+                } else {
+                    TokenType::Identifier(temp_string)
+                };
+
+                Some(Token::new(token_type, self.line, start_col))
+            } else if output_char.is_numeric() {
+                let start_col = self.col;
+
+                let mut temp_num = String::new();
+                temp_num.push(output_char);
+
+                while let Some(peeked) = self.input.peek() {
+                    if peeked.is_numeric() {
+                        temp_num.push(self.input.next().unwrap());
+                        self.col += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                Some(Token::new(
+                    TokenType::Int(temp_num.parse().unwrap()),
+                    self.line,
+                    start_col,
+                ))
+            } else {
+                let token_type = match output_char {
+                    '=' => TokenType::Assign,
+                    '+' => TokenType::Plus,
+                    ',' => TokenType::Comma,
+                    ';' => TokenType::Semicolon,
+                    '(' => TokenType::Lparen,
+                    ')' => TokenType::Rparen,
+                    '{' => TokenType::Lbrace,
+                    '}' => TokenType::Rbrace,
+                    '\n' => TokenType::NewLine,
+                    _ => todo!(),
+                };
+                Some(Token::new(token_type, self.line, self.col))
+            }
         } else {
             None
         }
@@ -42,23 +97,23 @@ impl<'a> Iterator for Lexer<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Token, TokenType};
+    use crate::{lexer::lexer, Token, TokenType};
 
     #[test]
     fn test_next_token() {
         let input = "=+(){},;";
         let expected = vec![
             Token::new(TokenType::Assign, 1, 1),
-            Token::new(TokenType::Plus, 2, 1),
-            Token::new(TokenType::Lparen, 3, 1),
-            Token::new(TokenType::Rparen, 4, 1),
-            Token::new(TokenType::Lbrace, 5, 1),
-            Token::new(TokenType::Rbrace, 6, 1),
-            Token::new(TokenType::Comma, 7, 1),
-            Token::new(TokenType::Semicolon, 8, 1),
+            Token::new(TokenType::Plus, 1, 2),
+            Token::new(TokenType::Lparen, 1, 3),
+            Token::new(TokenType::Rparen, 1, 4),
+            Token::new(TokenType::Lbrace, 1, 5),
+            Token::new(TokenType::Rbrace, 1, 6),
+            Token::new(TokenType::Comma, 1, 7),
+            Token::new(TokenType::Semicolon, 1, 8),
         ];
 
-        let lexed = expected.clone();
-        assert_eq!(lexed, expected);
+        let lexed = lexer::Lexer::new(input);
+        assert_eq!(lexed.collect::<Vec<_>>(), expected);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use lexer::token::{Token, TokenType};
 
 fn main() {
     // let testtoken = TokenType::Identifier(String::from("hello"));
-    let textlexer = lexer::lexer::Lexer::new("+=hello");
+    let textlexer = lexer::lexer::Lexer::new("+=hello123");
 
     println!("{:?}", textlexer.collect::<Vec<_>>());
 }
